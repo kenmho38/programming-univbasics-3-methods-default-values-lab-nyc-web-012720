d1d0cecf671cfe631e9514777115714c9c14e363
Ken Ho <kenmho38@hotmail.com>
2020-01-07 20:57:09 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,7 +2,7 @@
 # For output purposes, use "puts" instead of "print" or "p"
 
 def meal_choice(broccoli, macaroni, protien = "meat")
-  if protien = meat
+  if protien
   puts "What a nutritious meal!"
 else
   puts "A plate of #{protien} with #{veg1} and #{veg2}."
